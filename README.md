1d79da8db4b8463b7c04d2a4a3ec538b4766b0fe
theRealPadster <padspambox@gmail.com>
2021-07-23 22:48:33 -0400

    Add readme, better command handling

---
+++ README.md
@@ -0,0 +1,10 @@
+# Super simple Discord bot written in TypeScript
+_I don't do much yet but I'm learning_
+
+## Setup
+Create a .env file with `CLIENT_TOKEN="yourtoken"`
+
+## Development
+Run TypeScript watch task and use nodemon to apply changes in real time.
+1. `yarn run watch`
+2. `yarn dev`
--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,36 @@ import Discord from 'discord.js';
 
 const client = new Discord.Client();
 
+const PREFIX = '/';
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async (msg) => {
-  // msg.author
-  // if (msg.content === 'ping') {
-  //   msg.reply('Pong!');
-  // }
 
-  // Ignore messages unless start with /
-  if (!msg.content.startsWith('/')) return;
+  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
 
-  if (msg.content === '/hello') {
+  const args = msg.content.slice(PREFIX.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
+  if (command === 'hello') {
     try {
       await msg.react('👋');
-      await msg.reply('Hello! :)');
+      await msg.channel.send(`Hello ${msg.author}! :)`);
+      // await msg.reply(`Hello @${msg.author.username}! :)`);
+
     } catch (error) {
-      console.error('Something broke:', error);
+      console.error('Something broke:', error)
     }
-  }
+  }	else if (command === 'args-info') {
+		if (!args.length) {
+			return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
+		}
+
+		msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	}
 });
 
-
 //make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
+client.login(process.env.CLIENT_TOKEN);
